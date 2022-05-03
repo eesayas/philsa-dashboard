a43b1bbea0f9f798ba4e9fe90c2435f418e4769f
briones <briones@ualberta.ca>
2022-05-02 22:13:39 -0600

    chore: updated yml with secrets

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "philsa-db"),
+      user: env("DATABASE_USERNAME", "philsa"),
       password: env("DATABASE_PASSWORD"),
     },
     useNullAsDefault: true,
