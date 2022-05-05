6059efbbb4cea13ef258017a941470c243311573
briones <briones@ualberta.ca>
2022-05-04 22:23:50 -0600

    chore: size up uploads

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,10 +5,20 @@ module.exports = [
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
-  "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 50000 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   {
     name: "strapi::security",
     config: {
